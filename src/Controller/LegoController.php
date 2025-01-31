066a809c228c29c6a41c0f7ca17e617e0ec91c03
P2Wdisabled <louis1.potevin1@gmail.com>
2025-01-31 09:26:15 +0100

    adding database

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -4,31 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Lego;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\LegoService;
 
 class LegoController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(LegoService $legoService): Response
     {
-        // Création de l'objet Lego
-        $lego = new Lego(
-            10252,
-            'La coccinelle Volkwagen',
-            'Creator Expert'
-        );
-
-        // Hydratation des propriétés
-        $lego->setDescription("Construis une réplique LEGO® Creator Expert de l'automobile la plus populaire au monde. Ce magnifique modèle LEGO est plein de détails authentiques qui capturent le charme et la personnalité de la voiture, notamment un coloris bleu ciel, des ailes arrondies, des jantes blanches avec des enjoliveurs caractéristiques, des phares ronds et des clignotants montés sur les ailes.");
-        $lego->setPrice(94.99);
-        $lego->setPieces(1167);
-        $lego->setBoxImage('LEGO_10252_Box.png'); // Chemin relatif depuis le dossier public
-        $lego->setLegoImage('LEGO_10252_Main.jpg');
+        // Utilisation du service pour obtenir un objet Lego
+        $legos = $legoService->getLegos();
 
         // Passage de l'objet au template
         return $this->render('lego.html.twig', [
-            'lego' => $lego,
+            'legos' => $legos,
         ]);
     }
 
@@ -37,4 +26,17 @@ class LegoController extends AbstractController
     {
         return new Response("Achat du Lego avec l'ID : " . $id);
     }
+
+    #[Route('/{collection}', name: 'filter_by_collection', requirements: ['collection' => 'creator|star wars|creator expert'])]
+    public function filter(string $collection, LegoService $legoService): Response
+    {
+        // Récupérer les Legos filtrés par collection
+        $legos = $legoService->getLegosByCollection($collection);
+
+        // Passer les Legos au template
+        return $this->render('lego.html.twig', [
+            'legos' => $legos,
+        ]);
+    }
+
 }
